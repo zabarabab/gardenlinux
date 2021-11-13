23ecb6ab4e1669a6358490c393e667699a7d1908
Vincent Riesop <v.riesop@sap.com>
2021-11-13 15:25:06 +0000

    WIP

--- docker/prepare-kernel-build.sh
+++ docker/prepare-kernel-build.sh
@@ -20,7 +20,7 @@ while test $# -gt 0; do
       		debug=1
 	;;
 	--keepold) 
-		keepold=0	# Do not copy kernel scripts again (for debugging)  
+		keepold=1	# Do not copy kernel scripts again (for debugging)  
 	;;
 	-o|--output-dir*)
 		outputDir=`echo $1 | sed -e 's/^[^=]*=//g'`
@@ -44,26 +44,34 @@ mkdir -p build-kernel/linux
 
 
 # Copy kernel packages to container build environment
-if [ ! -f ${bundleDir}/.helper ] && [ ${keepold} == 0 ]; then
-	cp ../packages/manual/.helper build-kernel/linux/
+if [ ! -f ${bundleDir}/.helper ] || [ ${keepold} == 0 ]; then
+	echo "Getting fresh helper"
+	cp ../packages/manual/.helper ${bundleDir}
 fi
 
-if [ ! -f ${bundleDir}/LINUX-VERSION ] && [ ${keepold} == 0 ]; then
-	cp ../packages/manual/LINUX-VERSION build-kernel/linux/
+if [ ! -f ${bundleDir}/LINUX-VERSION ] || [ ${keepold} == 0 ]; then
+	echo "Getting fresh LINUX-VERSION"
+	cp ../packages/manual/LINUX-VERSION ${bundleDir} 
 fi
 
-if [ ! -f ${bundleDir}/.kernel-helper ] && [ ${keepold} == 0 ]; then
-	cp ../packages/manual/.kernel-helper build-kernel/linux/
+if [ ! -f ${bundleDir}/gpgkeys ] || [ ${keepold} == 0 ]; then
+	echo "Getting fresh gpgkeys"
+	cp ../packages/manual/linux-5.10.d/gpgkeys ${bundleDir}
+fi
+
+if [ ! -f ${bundleDir}/.kernel-helper ] || [ ${keepold} == 0 ]; then
+	echo "Getting fresh kernel-helper"
+	cp ../packages/manual/.kernel-helper ${bundleDir}
 fi
 
 if [[ ! -d ${bundleDir}/linux-5.10.d/ ]]; then
 	# just Copy everything matching linux-* again if the folder is not present
-	cp -r ../packages/manual/linux-* build-kernel/linux/
+	cp -r ../packages/manual/linux-* ${bundleDir}
 fi
 
 
 if [[ ! -d ${bundleDir}/cert/ ]]; then
-	cp -r ../cert build-kernel/cert/
+	cp -r ../cert ${bundleDir}
 fi
 
 
@@ -76,7 +84,8 @@ cd build-kernel/
 #gpg2 --locate-keys torvalds@kernel.org gregkh@kernel.org 514B0EDE3C387F944FB3799329E574109AEBFAAA
 #gpg --import cert/sign.pub > /dev/null
 
-import_gpg_keys
+echo "--------------------------"
+import_gpg_keys ${bundleDir}/gpgkeys
 
 get_kernel_sources 
 

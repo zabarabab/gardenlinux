073f008a2e2545518dcd23fa2fc5e2235727fa79
Vincent Riesop <v.riesop@sap.com>
2021-11-15 10:55:31 +0000

    WIP: patch template

--- packages/linux-template.d/download-template.sh
+++ packages/linux-template.d/download-template.sh
@@ -4,8 +4,6 @@ print_help() {
       echo "options:"
       echo "-h, --help			show brief help"
       echo "-d, --debug			debug mode of this script"
-      echo "-k, --keepold		do not copy kernel scripts again from packages (for debugging)"	
-
 }
 
 while test $# -gt 0; do
@@ -17,9 +15,6 @@ while test $# -gt 0; do
 	-d|--debug)
 		shift
       		debug=1
-	;;
-	--keepold) 
-		keepold=1	# Do not copy kernel scripts again (for debugging)  
 	;;
     *) print_help
       break
@@ -34,7 +29,6 @@ fi
 
 thisDir=$(dirname $0)
 kernelSrcDir=${kernelSrcDir:-"${thisDir}/gardenkernel-src"}
-keepold=${keepold:-0}
 mkdir -p ${kernelSrcDir}
 
 . ${thisDir}/LINUX-VERSION
@@ -47,15 +41,15 @@ mkdir -p ${kernelSrcDir}
 echo "--------------------------"
 import_gpg_keys ${kernelSrcDir} ${thisDir}/gpgkeys
 
-get_kernel_sources ${kernelSrcDir}
+get_kernel_sources ${kernelSrcDir} 
 
-get_debian_release_env ${kernelSrcDir}
+get_debian_release_env ${kernelSrcDir} 
 
-get_old_kernel ${kernelSrcDir}
+get_old_kernel ${kernelSrcDir} 
 
-get_ufs5_from_upstream ${kernelSrcDir}
+get_ufs5_from_upstream ${kernelSrcDir} 
 
-get_linux_stable_for_comments ${kernelSrcDir}
+get_linux_stable_for_comments ${kernelSrcDir} 
 
 
 

95b6efb3892371e320eff8cdf7f0b236dee9a262
Vincent Riesop <v.riesop@sap.com>
2021-12-20 09:30:48 +0000

    Workaround: use manual package linu script for dfl
    * copy patches and append them to orig debian series

--- packages/manual/linux.d/fpga-dfl-driver/genpatchset.sh
+++ packages/manual/linux.d/fpga-dfl-driver/genpatchset.sh
@@ -4,18 +4,21 @@ set -euxo pipefail
 workingDir="$(readlink -f "$(pwd)")"
 srcDir="linux-dfl"
 DFL_BRANCH="fpga-ofs-dev-5.10-lts"
-KERNEL_VERSION="5.10.81"
+KERNEL_VERSION="5.10.84"
+DFL_TAG="2.0.0-1"
 
-git clone https://github.com/OPAE/linux-dfl.git ${srcDir}
+#git clone https://github.com/OPAE/linux-dfl.git ${srcDir}
 
 pushd ${srcDir} || exit 1
 
-    git remote add linux-stable git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
+#    git remote add linux-stable git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
     git fetch -t linux-stable
-    git checkout ${DFL_BRANCH}
+    git checkout ${DFL_BRANCH} 
     git format-patch -o dfl_patches --first-parent --no-merges v${KERNEL_VERSION}..${DFL_BRANCH}
-    mv dfl_patches ${workingDir}
-
+    mv dfl_patches ${workingDir}/dfl_patches_branch
+    git checkout ${DFL_TAG} 
+    git format-patch -o dfl_patches --first-parent --no-merges v${KERNEL_VERSION}..${DFL_TAG}
+    mv dfl_patches ${workingDir}/dfl_patches_tag
 popd ||exit 1
 
-rm -rf ${srcDir}
+#rm -rf ${srcDir}
